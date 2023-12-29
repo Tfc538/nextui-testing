2d85ef7711e3f8268dcf58ca794d5c619a0ffd73
TFCSARSS <60168596+Tfc538@users.noreply.github.com>
2023-12-29 02:11:59 +0100

    Updated Config

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
 	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	description: "Next.js + NextUI = ❤️",
 	navItems: [
 		{
 			label: "Home",
@@ -64,6 +64,6 @@ export const siteConfig = {
 		twitter: "https://twitter.com/getnextui",
 		docs: "https://nextui.org",
 		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
+    	sponsor: "https://patreon.com/jrgarciadev"
 	},
 };
